a639803d891355dc02d05f21f7f936851f24c8f6
jobm <jobmwaniki18@gmail.com>
2015-10-09 12:16:29 +0300

    collected and separeatd static files into assets

--- first_app/urls.py
+++ first_app/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$','home.views.index',name='home'),
-    url(r'^feedback/$','home.views.feedback',name='feedback'),
+    url(r'^$', 'home.views.index', name='home'),
+    url(r'^feedback/$', 'home.views.feedback', name='feedback'),
     url(r'^admin/', include(admin.site.urls)),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=STATIC_ROOT)
--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from home.models import Student,ClassRoom
+from home.models import Student, ClassRoom
 from .forms import StudentForm
 #define student admin
+
+
 class StudentAdmin(admin.ModelAdmin):
+
     form = StudentForm
+
 # Register your models here.
-admin.site.register(Student,StudentAdmin)
+admin.site.register(Student, StudentAdmin)
 admin.site.register(ClassRoom)
